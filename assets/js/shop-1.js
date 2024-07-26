b2c691235e39ac103f1586404ec20ef87193d7bd
shiv1819 <shivampanchal11q@gmail.com>
2024-07-26 19:46:02 +0530

    Files Uploaded

---
+++ assets/js/shop-1.js
@@ -0,0 +1,85 @@
+var x = 0;
+
+function filter() {
+    if (window.innerWidth > 767) {
+        x++;
+        if (x % 2 == 1) {
+            document.getElementById("show").innerHTML = "Show Filters";
+            document.getElementById("filter").style.transition = "1s";
+            document.getElementById("filter").style.width = "0";
+            document.getElementById("filter").style.transform = "translateX(-1000px)";
+            document.getElementById("filter").style.opacity = "0";
+            document.getElementById("filter").style.padding = "0";
+            document.getElementById("id1").style.width = "25%";
+            document.getElementById("id2").style.width = "25%";
+            document.getElementById("id3").style.width = "25%";
+            document.getElementById("id4").style.width = "25%";
+            document.getElementById("id4").style.width = "25%";
+            document.getElementById("id5").style.width = "25%";
+            document.getElementById("id6").style.width = "25%";
+            document.getElementById("id7").style.width = "25%";
+            document.getElementById("id8").style.width = "25%";
+            document.getElementById("id9").style.width = "25%";
+            document.getElementById("id10").style.width = "25%";
+            document.getElementById("id11").style.width = "25%";
+            document.getElementById("id12").style.width = "25%";
+            document.getElementById("id13").style.width = "25%";
+            document.getElementById("id14").style.width = "25%";
+            document.getElementById("id15").style.width = "25%";
+            document.getElementById("id16").style.width = "25%";
+            document.getElementById("id17").style.width = "25%";
+            document.getElementById("id18").style.width = "25%";
+            document.getElementById("id19").style.width = "25%";
+            document.getElementById("id20").style.width = "25%";
+            console.log(x);
+        }
+        else if (x % 2 == 0) {
+            document.getElementById("show").innerHTML = "Hide Filters";
+            document.getElementById("filter").style.transition = "1s";
+            document.getElementById("filter").style.width = "30%";
+            document.getElementById("filter").style.transform = "translateX(0px)";
+            document.getElementById("filter").style.opacity = "1";
+            document.getElementById("filter").style.paddingLeft = "70px";
+            document.getElementById("id1").style.width = "33%";
+            document.getElementById("id2").style.width = "33%";
+            document.getElementById("id3").style.width = "33%";
+            document.getElementById("id4").style.width = "33%";
+            document.getElementById("id4").style.width = "33%";
+            document.getElementById("id5").style.width = "33%";
+            document.getElementById("id6").style.width = "33%";
+            document.getElementById("id7").style.width = "33%";
+            document.getElementById("id8").style.width = "33%";
+            document.getElementById("id9").style.width = "33%";
+            document.getElementById("id10").style.width = "33%";
+            document.getElementById("id11").style.width = "33%";
+            document.getElementById("id12").style.width = "33%";
+            document.getElementById("id13").style.width = "33%";
+            document.getElementById("id14").style.width = "33%";
+            document.getElementById("id15").style.width = "33%";
+            document.getElementById("id16").style.width = "33%";
+            document.getElementById("id17").style.width = "33%";
+            document.getElementById("id18").style.width = "33%";
+            document.getElementById("id19").style.width = "33%";
+            document.getElementById("id20").style.width = "33%";
+            console.log(x);
+
+        }
+    }
+}
+function phone() {
+    x++;
+
+    if (x % 2 == 1) {
+        document.getElementById("show-phone").innerHTML = "Hide Filters";
+        document.getElementById("filter").style.width = "100%";
+        document.getElementById("filter").style.transform = "translateY(0px)";
+        document.getElementById("filter").style.padding = "0";
+    }
+    if (x % 2 == 0) {
+        document.getElementById("show-phone").innerHTML = "Filters";
+        document.getElementById("filter").style.width = "100%";
+        document.getElementById("filter").style.transform = "translateY(-1000px)";
+        document.getElementById("filter").style.padding = "0";
+    }
+
+}
